ca2ac0b3c01271d34b5cbbe6b040cb09131c938a
KevinGutej <76398856+KevinGutej@users.noreply.github.com>
2023-09-02 18:27:32 +0200

    Adding More html

--- generate.php
+++ generate.php
@@ -4,4 +4,27 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $name = $_POST['name'];
     $email = $_POST['email'];
     $bio = $_POST['bio'];
+
+        $portfolioHTML = <<<HTML
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>$name's Portfolio</title>
+            <link rel="stylesheet" href="style.css">
+        </head>
+        <body>
+            <header>
+                <h1>$name</h1>
+                <p>Email: $email</p>
+            </header>
+            <main>
+                <section class="bio">
+                    <h2>About Me</h2>
+                    <p>$bio</p>
+            </main>
+        </body>
+        </html>
+        HTML;
 }
