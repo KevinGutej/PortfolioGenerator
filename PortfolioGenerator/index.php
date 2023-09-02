0977031d16feefed354ae41df99dced8382880f4
KevinGutej <76398856+KevinGutej@users.noreply.github.com>
2023-09-02 18:22:44 +0200

    Creating form / template HTML

---
+++ PortfolioGenerator/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio Generator</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <h1>Portfolio Generator</h1>
+    <form action="generate.php" method="POST">
+        <label for="name">Name:</label>
+        <input type="text" name="name" required><br>
+
+        <label for="email">Email:</label>
+        <input type="email" name="email" required><br>
+    </form>
+</body>
+
+</html>
